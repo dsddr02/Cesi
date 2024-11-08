753caa1bdde4d4970aa2e3eb5704daa65a0e5f54
fewewf <32592150+fewewf@users.noreply.github.com>
2024-11-08 08:34:20 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -4,8 +4,8 @@
 sed -i 's/192.168.1.1/192.168.110.235/g' package/base-files/files/bin/config_generate
 
 # Remove packages
-rm -rf feeds/luci/applications/luci-app-passwall
+# rm -rf feeds/luci/applications/luci-app-passwall
 
 # Add packages
-git clone https://github.com/nantayo/My-Pkg package/my-pkg
+# git clone https://github.com/nantayo/My-Pkg package/my-pkg
 git clone --depth=1 https://github.com/ophub/luci-app-amlogic package/amlogic
